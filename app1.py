0235ee1c435697d9fa4a6d41687b786415e7e338
Anubhav Sood <anubhavsood18@gmail.com>
2024-06-29 17:35:12 +0530

    done

--- app1.py
+++ app1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle as pkl
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 # import sklearn 
 # Load the data
 df_1 = pd.read_csv('first_telc.csv')
